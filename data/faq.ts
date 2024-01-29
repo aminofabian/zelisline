bc2be78dcaace93389f4b6b792cd2abb340f57b0
fabianamino <aminofabian@gmail.com>
2024-01-29 08:06:38 +0300

    added faqs for web design

--- data/faq.ts
+++ data/faq.ts
@@ -2,48 +2,76 @@
 export const items = [
   {
     value: 'item-1',
-    triggerText: '❈ Has anyone recovered from the Google algorithm updates from September? If so, what did you do?',
-    content: "\nI'm with you.My best website(10 years old, REAL handwritten content and handmade animations) got obliterated.All my niche site competitors also got hammered. I havent figured out what to do yet, still trying to publish more content attempting to tighten up my topic clusters, but beyond that nothing has worked yet. \nI have two sites that are doing well, although they are less than a year old, and the biggest difference between them and my sites that got hammered is they have STRONG EEAT and tighter topical coverage."
+    triggerText: '❈ How Much Do You Charge for a Website?',
+    content: `It depends. It depends on the complexity of your project and the features you require.
+
+    We understand that every business is unique and may have different needs, so our prices are determined on a case-by -case basis.Our packages start at kes20K($200) and can go up to kes5m($50,000) or more, depending on the scope of work.
+
+    We suggest you talk to us directly about your project so we can understand your requirements and provide you with a customized quote.
+    
+    We\'ve also written a detailed guide on how much it might cost to build a website, which you can refer to as a general reference.
+    
+    Our team of experts will always work with you to find the best solution that fits your budget and meets your business needs.Whether you\'re looking for a simple brochure-style website, a complex e-commerce platform, or a SaaS project, we have the experience and expertise to bring it to life.`
   },
   {
     value: 'item-2',
-    triggerText: '❈ I Checked 100 Mediavine Sites - 95% of Them Were Hit By HCU',
-    content: 'I\'m with you.My best website(10 years old, REAL handwritten content and handmade animations) got obliterated.All my niche site competitors also got hammered. I havent figured out what to do yet, still trying to publish more content attempting to tighten up my topic clusters, but beyond that nothing has worked yet. I have two sites that are doing well, although they are less than a year old, and the biggest difference between them and my sites that got hammered is they have STRONG EEAT and tighter topical coverage.',
+    triggerText: '❈ Why Are Some of Your Projects So Expensive?',
+    content: `It depends on the scope of the project or the amount of work we pump into the project. 
+A more complex website or platform will require more time, effort, and resources to develop and maintain, which is reflected in the price.
+Most agencies use templates and pre- made designs to cut costs, but we believe in creating unique solutions tailored specifically for our clients.Some of our clients require extensive customizations and integrations, which can drive up the cost.
+  However, we always try to find a balance between quality and affordability for our clients.Our goal is to provide exceptional services that deliver tangible results and add value to your business.`,
   },
   {
     value: 'item-3',
-    triggerText: ' ❈   Is it accessible?',
-    content: 'I\'m with you.My best website(10 years old, REAL handwritten content and handmade animations) got obliterated.All my niche site competitors also got hammered. I havent figured out what to do yet, still trying to publish more content attempting to tighten up my topic clusters, but beyond that nothing has worked yet. I have two sites that are doing well, although they are less than a year old, and the biggest difference between them and my sites that got hammered is they have STRONG EEAT and tighter topical coverage.',
+    triggerText: ' ❈   How Long Does It Take to Build a Website?',
+    content: `Again, it depends on the complexity of your project. A simple website can take anywhere from a week to 6 weeks, while a more complex one can take up to several months.
+Our development process includes initial consultations to understand your business and requirements, design mockups for your approval, and then the actual coding and testing phase. We also provide regular updates and feedback sessions to ensure that we are meeting your expectations.
+Our team is dedicated and efficient, but we also prioritize quality over speed. We want to make sure that your website not only looks great but also functions seamlessly for your customers.`,
   },
   {
     value: 'item-4',
-    triggerText: '❈  Is it accessible?',
-    content: 'I\'m with you.My best website(10 years old, REAL handwritten content and handmade animations) got obliterated.All my niche site competitors also got hammered. I havent figured out what to do yet, still trying to publish more content attempting to tighten up my topic clusters, but beyond that nothing has worked yet. I have two sites that are doing well, although they are less than a year old, and the biggest difference between them and my sites that got hammered is they have STRONG EEAT and tighter topical coverage.',
+    triggerText: '❈  Are There Any Ongoing Costs After the Website is Launched?',
+    content: `Yes, there may be ongoing costs such as hosting fees, domain fees, and maintenance costs. These are necessary to keep your website running smoothly and up-to-date with the latest security measures.
+We also offer ongoing support packages for our clients who require regular updates or maintenance on their websites. This ensures that your website stays current and relevant in this ever-changing digital landscape.
+Frameworks change, technologies evolve, and your business may require new features down the line. We are always here to help you adapt and grow with these changes.`,
   },
   {
     value: 'item-5',
-    triggerText: '❈  Is it accessible?',
-    content: 'I\'m with you.My best website(10 years old, REAL handwritten content and handmade animations) got obliterated.All my niche site competitors also got hammered. I havent figured out what to do yet, still trying to publish more content attempting to tighten up my topic clusters, but beyond that nothing has worked yet. I have two sites that are doing well, although they are less than a year old, and the biggest difference between them and my sites that got hammered is they have STRONG EEAT and tighter topical coverage.',
+    triggerText: '❈  What Will You Require from Me to Build a Website?',
+    content: `We will require your input and feedback throughout the development process. We will also need any content or media assets you may want to include on your website, such as images, videos, and text.
+Additionally, we may also ask for access to your domain and hosting accounts so we can set up your website correctly. Don't worry, we take extra precautions to protect your sensitive information and ensure the security of your website.
+We may request for your team images, or if you don't have one (and are located in Kenya), organize with our photographer to come and take the photos at a small added cost.`,
   },
   {
     value: 'item-6',
-    triggerText: '❈  Is it accessible?',
-    content: 'I\'m with you.My best website(10 years old, REAL handwritten content and handmade animations) got obliterated.All my niche site competitors also got hammered. I havent figured out what to do yet, still trying to publish more content attempting to tighten up my topic clusters, but beyond that nothing has worked yet. I have two sites that are doing well, although they are less than a year old, and the biggest difference between them and my sites that got hammered is they have STRONG EEAT and tighter topical coverage.',
+    triggerText: `❈  What if I Don't LIke the Website You Build for Me ?`,
+    content: `We work closely with our clients throughout the development process to ensure that we understand and meet their expectations. We also provide regular updates and feedback sessions so you can see the progress of your website and give any necessary input.
+We'll begin by providing a simple mockup design, and we'll make unlimited revisions until you are satisfied with the final product. Our goal is to create a website that not only meets your business needs but also aligns with your vision and brand.
+In the unlikely event that you are dissatisfied with our work, we have a satisfaction guarantee policy and will work with you to resolve any issues or concerns. Your satisfaction is our top priority, and we want to make sure you are happy with the end result. 
+We understand that investing in a website for your business can be a big decision, and we're here to help guide you through the process. Our team of experts is dedicated to providing exceptional services at competitive prices.`,
   },
   {
     value: 'item-7',
-    triggerText: '❈  Is it accessible?',
-    content: 'I\'m with you.My best website(10 years old, REAL handwritten content and handmade animations) got obliterated.All my niche site competitors also got hammered. I havent figured out what to do yet, still trying to publish more content attempting to tighten up my topic clusters, but beyond that nothing has worked yet. I have two sites that are doing well, although they are less than a year old, and the biggest difference between them and my sites that got hammered is they have STRONG EEAT and tighter topical coverage.',
+    triggerText: `❈  What's Your Payment Schedule?`,
+    content: `We usually require a deposit before starting any project, and the remaining balance can be paid in increments throughout the development process or upon completion. Pay at least 50% before we start work and the remaining 50% after 30 days.
+30 days because some projects may take longer than anticipated, and there might be delays on your end in providing necessary materials or feedback. We want to make sure that we have enough time to deliver the best quality work for you.
+We also offer installment plans for larger projects, so you can pay in smaller amounts over a longer period.
+Our payment options are flexible and can be tailored to fit your budget and timeline. Our priority is to provide you with a smooth and stress-free development process, and we're willing to work with you to find a payment schedule that works best for both of us. `,
   },
   {
     value: 'item-8',
-    triggerText: '❈  Is it accessible?',
-    content: 'I\'m with you.My best website(10 years old, REAL handwritten content and handmade animations) got obliterated.All my niche site competitors also got hammered. I havent figured out what to do yet, still trying to publish more content attempting to tighten up my topic clusters, but beyond that nothing has worked yet. I have two sites that are doing well, although they are less than a year old, and the biggest difference between them and my sites that got hammered is they have STRONG EEAT and tighter topical coverage.',
+    triggerText: `❈  Who Hosts the Website?`,
+    content: `We offer hosting services for our clients or can work with your preferred hosting provider. Our team will handle the setup and maintenance of your website on the chosen hosting platform.
+If you choose to use our hosting services, we guarantee fast loading times, high-security measures, and reliable support for any technical issues that may arise. We also provide regular backups of your website to ensure its safety and stability.
+However, if you prefer to use your own hosting provider, we will work with them to ensure the proper setup of your website. Our goal is to make the process as seamless as possible for our clients. `,
   },
   {
     value: 'item-9',
-    triggerText: '❈  Is it accessible?',
-    content: 'I\'m with you.My best website(10 years old, REAL handwritten content and handmade animations) got obliterated.All my niche site competitors also got hammered. I havent figured out what to do yet, still trying to publish more content attempting to tighten up my topic clusters, but beyond that nothing has worked yet. I have two sites that are doing well, although they are less than a year old, and the biggest difference between them and my sites that got hammered is they have STRONG EEAT and tighter topical coverage.',
+    triggerText: `❈  Can I update My Website Once It's Launched?`,
+    content: `Yes, you can update your website once it has been launched. We provide training and support for our clients on how to make simple updates or changes to their websites.
+We can work with a CMS such as WordPress, Joomla, or Drupal, depending on your preferences and needs.These platforms allow you to easily update content, add new pages, and make other changes without needing advanced coding skills.
+For more advanced websites that we've built from scratch, we can provide a customized admin panel that you can use to make updates or changes.
+We also offer ongoing support packages for clients who require regular updates but may not have the time or resources to do it themselves.We understand that your business needs may change and evolve, and we're here to help you adapt and grow with those changes.`,
   }
 ];
 
